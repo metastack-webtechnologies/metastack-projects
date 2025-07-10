c74f481b806716d9349798330f34129299fc8150
Wartcher <divyenm99@gmail.com>
2025-07-10 11:21:31 +0530

    feat: Initial setup and dependency fixes for AI Todo App (Django & React)

--- ai-todo-app/client/dev-dist/sw.js
+++ ai-todo-app/client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-bea60542'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o8dvj24chlg"
+    "revision": "0.c9adj0bgms8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
